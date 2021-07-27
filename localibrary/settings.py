796a66ebedf44dbe92615827e7048183525af55c
Geoffrey Sagini Mogambi <saginigeoffrey@gmail.com>
2021-07-27 06:20:35 -0400

    Update settings.py

--- localibrary/settings.py
+++ localibrary/settings.py
@@ -127,7 +127,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'saginigeoffrey@gmail.com'
-EMAIL_HOST_PASSWORD = 'jvbvmjyiieoqupse'
+EMAIL_HOST_PASSWORD = 'htrfdttfggggghhyt'
 EMAIL_USE_TLS = True
 
 # Static files (CSS, JavaScript, Images)
